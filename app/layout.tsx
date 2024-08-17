940267c433e0c9e07a869bc2fd04555a28b8f6bc
udaysarkarud <udaysarkarud@gmail.com>
2024-08-18 00:24:09 +0600

    added gntag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -20,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-2JCGZXZ3DQ" />
     </html>
   );
 }
